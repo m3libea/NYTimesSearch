6383be2f4d2f27a3629c773bb51d2e4e7a330f35
Rocio Romero <rociorommor@gmail.com>
2017-09-20 11:44:34 -0700

    Created object to handle queries

---
+++ app/src/main/java/com/m3libea/nytimessearch/models/SearchQuery.java
@@ -0,0 +1,122 @@
+package com.m3libea.nytimessearch.models;
+
+import android.text.TextUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by m3libea on 9/20/17.
+ */
+
+public class SearchQuery {
+
+
+    private enum SortDir{
+        NEWEST,
+        OLDEST
+    }
+
+
+    private String query;
+    private Date beginDate = null;
+    private SortDir sort = null;
+    private int page = 0;
+    private List<String> newsDesks = null;
+
+    public SearchQuery(String query, Date beginDate, SortDir sort, int page, List<String> newsDesks) {
+        this.query = query;
+        this.beginDate = beginDate;
+        this.sort = sort;
+        this.page = page;
+        this.newsDesks = newsDesks;
+    }
+
+    public SearchQuery(String query) {
+        this.query = query;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public Date getBeginDate() {
+        return beginDate;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public List<String> getNewsDesks() {
+        return newsDesks;
+    }
+
+    public SortDir getSort() {
+        return sort;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public void setBeginDate(Date beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    public void setSort(SortDir sort) {
+        this.sort = sort;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public void setNewsDesks(List<String> newsDesks) {
+        this.newsDesks = newsDesks;
+    }
+
+    public String getFormattedDate(){
+        SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");
+        return beginDate == null ? null : df.format(beginDate);
+    }
+
+    public String getFormattedSort(){
+        String s = null;
+
+        if (sort != null) {
+            switch (sort){
+                case NEWEST:
+                    s = "newest";
+                    break;
+                case OLDEST:
+                    s = "oldest";
+                    break;
+            }
+        }
+
+        return s;
+    }
+
+    public String getFormattedDesks(){
+
+        if (newsDesks == null) {
+            return null;
+        }
+
+        List<String> aux = new ArrayList<>();
+
+        for (String s: newsDesks){
+            aux.add("\"s\"");
+        }
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("new_desk:(");
+        sb.append(TextUtils.join(" ", aux));
+        sb.append(")");
+        return sb.toString();
+    }
+
+}
