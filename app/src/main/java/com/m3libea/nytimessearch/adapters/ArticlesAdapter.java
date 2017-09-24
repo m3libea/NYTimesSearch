d88c4a529d8624f6bea634d8c013f93b4e7c69c4
Rocio Romero <rociorommor@gmail.com>
2017-09-23 22:27:59 -0700

    Heterogenous layouts

--- app/src/main/java/com/m3libea/nytimessearch/adapters/ArticlesAdapter.java
+++ app/src/main/java/com/m3libea/nytimessearch/adapters/ArticlesAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,9 +23,9 @@ import java.util.List;
  * Created by m3libea on 9/23/17.
  */
 
-public class ArticlesAdapter extends RecyclerView.Adapter<ArticlesAdapter.ViewHolder> {
+public class ArticlesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ImageViewHolder extends RecyclerView.ViewHolder{
 
        //@BindView(R.id.ivimage)
         ImageView ivImage;
@@ -37,7 +36,7 @@ public class ArticlesAdapter extends RecyclerView.Adapter<ArticlesAdapter.ViewHo
 
         CardView card;
 
-        public ViewHolder(View itemView) {
+        public ImageViewHolder(View itemView) {
             super(itemView);
 
             ivImage = (ImageView) itemView.findViewById(R.id.ivimage);
@@ -50,15 +49,43 @@ public class ArticlesAdapter extends RecyclerView.Adapter<ArticlesAdapter.ViewHo
             tvTitle.setText(article.getHeadline().getMain());
             tvDesks.setText(article.getNewsDesk());
 
-            String thumbnail = "";
+            String thumbnail = "http://www.nytimes.com/" + article.getMultimedia().get(0).getUrl();
 
-            if (article.getMultimedia().size() > 0){
-                thumbnail = "http://www.nytimes.com/" + article.getMultimedia().get(0).getUrl();
-            }
+            Glide.with(context).load(thumbnail).into(ivImage);
 
-            if(!TextUtils.isEmpty(thumbnail)){
-                Glide.with(context).load(thumbnail).into(ivImage);
-            }
+            card.setOnClickListener(view -> {
+                //Create intent
+                Intent i = new Intent(context, ArticleActivity.class);
+                //pass in that article into intent
+                i.putExtra("article", Parcels.wrap(article));
+                //launch activity
+                context.startActivity(i);
+            });
+        }
+    }
+
+
+    public class TitleViewHolder extends RecyclerView.ViewHolder{
+
+        //@BindView(tvTitle)
+        TextView tvTitle;
+        //@BindView(tvDesks)
+        TextView tvDesks;
+
+        CardView card;
+
+        public TitleViewHolder(View itemView) {
+            super(itemView);
+
+            tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
+            tvDesks = (TextView) itemView.findViewById(R.id.tvDesks);
+            card = (CardView) itemView.findViewById(R.id.card);
+        }
+
+        public void bind(final Doc article){
+            tvTitle.setText(article.getHeadline().getMain());
+            tvDesks.setText(article.getNewsDesk());
+
 
             card.setOnClickListener(view -> {
                 //Create intent
@@ -74,6 +101,9 @@ public class ArticlesAdapter extends RecyclerView.Adapter<ArticlesAdapter.ViewHo
     private List<Doc> articles;
     private Context context;
 
+    private final int IMAGE = 0, TITLE = 1;
+
+
     public ArticlesAdapter(Context context, List<Doc> articles) {
         this.articles = articles;
         this.context = context;
@@ -84,30 +114,59 @@ public class ArticlesAdapter extends RecyclerView.Adapter<ArticlesAdapter.ViewHo
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         Context context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
 
-        // Inflate the custom layout
-        View articleView = inflater.inflate(R.layout.item_article_result, parent, false);
+        RecyclerView.ViewHolder viewHolder;
 
-        // Return a new holder instance
-        ViewHolder viewHolder = new ViewHolder(articleView);
+        switch (viewType){
+            case TITLE:
+                // Inflate the custom layout
+                View titleView = inflater.inflate(R.layout.item_article_result_title, parent, false);
+                // Return a new holder instance
+                viewHolder = new TitleViewHolder(titleView);
+                break;
+            default:
+                // Inflate the custom layout
+                View imageView = inflater.inflate(R.layout.item_article_result, parent, false);
+                // Return a new holder instance
+                viewHolder = new ImageViewHolder(imageView);
+                break;
+        }
+
         return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(ArticlesAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
 
         Doc article = articles.get(position);
 
-        holder.bind(article);
+        switch (holder.getItemViewType()){
+            case TITLE:
+                TitleViewHolder tHolder = (TitleViewHolder) holder;
+                tHolder.bind(article);
+                break;
+            default:
+                ImageViewHolder iHolder = (ImageViewHolder) holder;
+                iHolder.bind(article);
+                break;
+        }
     }
 
     @Override
     public int getItemCount() {
         return articles.size();
     }
+
+    @Override
+    public int getItemViewType(int position) {
+        Doc article = articles.get(position);
+
+        return article.getMultimedia().size() > 0 ? IMAGE : TITLE;
+    }
+
 
 
 }
